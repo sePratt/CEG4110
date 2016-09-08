6e5b002c40fe8df665c760f3d3dc6fdb84f86849
Sydney Pratt <pratt.35@wright.edu>
2016-09-08 16:41:12 -0400

    Moved files back to tutorial plus new files

---
+++ tutorial/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+<<<<<<< HEAD
+	public static void main(String args[]){
+		System.out.println("Hello my aggravating friend.");
+=======
+	public dynamic void main(String args[]){
+		system.out.printline("See you CEG 4110!");
+
+>>>>>>> parent of 431fae7... Revert "I'm so smart i commited this without testing."
+}
+}
